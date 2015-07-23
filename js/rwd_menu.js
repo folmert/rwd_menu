857e4db8075341b270338be4ddbb3fdbdc22006f
folmert <folmert2@gmail.com>
2015-07-23 22:13:31 +0200

    cosmetics

--- js/rwd_menu.js
+++ js/rwd_menu.js
@@ -1,21 +1,20 @@
-var menu = document.querySelectorAll('.rwd_menu')[0];
+"use strict";
 
-menu.addEventListener('click', function(e) {
+
+var rwd_menu = document.querySelectorAll('.rwd_menu')[0];
+
+rwd_menu.addEventListener('click', function(e) {
 	if( e.target === this ) {
 		this.classList.toggle('js-menu_opened');
 	}
 },false);
 
-
 var submenu = document.querySelectorAll('.rwd_menu li.parent');
 
-
-var index;
-for (index = 0; index < submenu.length; ++index) {
-	submenu[index].addEventListener('click', function(e) {
+var i;
+for (i = 0; i < submenu.length; ++i) {
+	submenu[i].addEventListener('click', function(e) {
 		this.classList.toggle('js-submenu_opened');
 	},false);
 	
 }
-
-
